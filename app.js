0ce9e73efe7fd2b01a01e36d9d4e7983efe90353
Aaron Shivers <aaron@moonmail.org>
2019-01-01 12:47:48 -0600

    Added EJS and files

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const { PORT } = process.env
 
 const userRoutes = require('./routes/user')
 
+app.set('view engine', 'ejs')
+
 app.use(helmet())
 app.use(express.json())
 
 app.use(userRoutes)
 
-app.get('/', (req, res) => res.send('Welcome to the Mongo Search App'))
+app.get('/', (req, res) => res.render('index'))
 
 app.use((req, res, next) => {
   res.status(404).send('Sorry, we cannot find that!')
--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,7 @@ router.post('/users', (req, res) => {
 
 router.get('/users', (req, res) => {
   User.find().then((users) => {
-    res.send(users)
+    res.render('users', { users })
   })
 })
 
@@ -23,40 +23,10 @@ router.post('/users/search', (req, res) => {
   const { query } = req.body
 
   User.find( { $text: { $search: query } } ).then((user) => {
-    console.log(query)
-    console.log(user)
     if (!user) return res.status(404).send(`Sorry, we couldn't find that.`)
     
     res.send(user)
   }).catch(err => res.status(500).send(err))
-
-  // User.textSearch('Matt', (err, result) => {
-  //   if (err) return res.status(500).send(err)
-
-  //   const inspect = require('util').inspect
-  //   console.log(inspect(result, { depth: null }))
-  // })
 })
-
-// router.post('/users/search', (req, res) => {
-//   const { query } = req.body
-
-//   User.textSearch(query, ((err, result) => {
-//     if (err) return handleError(err)
-
-//     const inspect = require('util').inspect
-
-//     console.log(inspect(result, { depth: null }))
-//     // res.send(result)
-//   })
-//   // User.textSearch(query).then((err, result) => {
-//   //   if (err) return handleError(err)
-
-//   //   const inspect = require('util').inspect
-
-//   //   console.log(inspect(result, { depth: null }))
-//   //   // res.send(result)
-//   // })
-// })
 
 module.exports = router
