a2b3cc3993e8049560cd9b57af892be6745529d6
Aaron Shivers <aaron@moonmail.org>
2019-01-01 10:18:14 -0600

    Added catch for error handling on POST /users

--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,13 @@ const router = express.Router()
 const User = require('../models/user')
 
 router.post('/users', (req, res) => {
-  const newUser = { email, password } = req.body
+  const { email, password } = req.body
+  const newUser = { email, password }
   const user = new User(newUser)
 
   user.save().then((user) => {
     res.status(201).send(user)
-  })
+  }).catch(err => res.status(400).send(err.message))
 })
 
 router.get('/users', (req, res) => {
